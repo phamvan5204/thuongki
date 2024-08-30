d3210bb5d13362412c6dec04132fdbecc8558909
phamvan5204 <vannn151@gmail.com>
2024-08-30 20:36:28 +0700

    Add files via upload

---
+++ STT73_22696641_PhamThiCamVan_99_tuần4_bài5.java
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//WC//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xh">
+
+<html> xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <title>Untitled Document</title>
+        <script language="javascript">
+        function cong2so()
+        ｛
+        var s01-parseInt(myform.txtso1.value);
+        var so2-parseInt (myform.txtso2.value);
+        var kq=501+502; document-write('ket qua:*+ kq);function cong2so()
+        ｛
+        
+        </script>
+        </head>
+        <body>
+        < form name="myform" >
+         <input type="text" name="txtso1" />
+         <input type="text" name="txtso2" />
+         <input type="button" value= "cộng" onclick="cong2so ()*/>
+        </form> 
+        </body>
+
+</html>
